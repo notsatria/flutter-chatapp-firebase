db96cb3e848273ab8d6b4a6b261c82e336cb4934
notsatria <buanasatriaa@gmail.com>
2023-06-19 22:53:38 +0700

    first commit, lack of features and design, will updating soon

---
+++ android/app/src/main/kotlin/com/example/chatting_starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatting_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:chatting_starter/firebase_options.dart';
+import 'package:chatting_starter/pages/chat_page.dart';
+import 'package:chatting_starter/pages/login_page.dart';
+import 'package:chatting_starter/pages/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting 2219',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginPage.id,
+      routes: {
+        LoginPage.id: (context) => const LoginPage(),
+        RegisterPage.id: (context) => const RegisterPage(),
+        ChatPage.id: (context) => const ChatPage(),
+      },
+    );
+  }
+}
